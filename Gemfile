d1970ca008a429d302362488d1ba61208f31555b
Elymar <jozzua@gmail.com>
2013-07-23 20:55:48 +0800

    Added Flickraw gem

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 ruby '2.0.0'
 gem 'rails', '4.0.0'
+gem 'flickraw'
 group :development, :test do
   gem 'sqlite3', '1.3.7'
   gem 'rspec-rails', '2.13.1'
--- spec/controllers/photosearch_controller_spec.rb
+++ spec/controllers/photosearch_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe PhotosearchController do
-
+  describe 'search photos' do
+    it 'assigns a new search' do
+      get :photosearch, search_term: 'kittens'
+      response.code.should == '200'
+    end
+  end
 end
