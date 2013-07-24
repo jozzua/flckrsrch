c707e4a091b846d6eeecd2de596b937256bf5277
Elymar <jozzua@gmail.com>
2013-07-25 06:12:08 +0800

    Basic search working

--- app/controllers/photosearch_controller.rb
+++ app/controllers/photosearch_controller.rb
@@ -5,6 +5,7 @@ class PhotosearchController < ApplicationController
   end
 
   def show
+    @searchterm = params[:search_term]
     @show_photos = flickr.photos.search(tags: params[:search_term], page: params[:page], per_page: 10, content_type: 1)
     add_url
   end
