57a3595036d517694c67443789bfe7f7abf86919
Elymar <jozzua@gmail.com>
2013-07-26 23:56:20 +0800

    Converted into single page app

--- app/controllers/photosearch_controller.rb
+++ app/controllers/photosearch_controller.rb
@@ -2,12 +2,9 @@ class PhotosearchController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:search, :show]
 
   def search
-  end
-
-  def show
-    @searchterm = params[:search_term]
-    session[:searchterm] = @searchterm
-    @show_photos = flickr.photos.search(text: params[:search_term], page: params[:page], per_page: 10, content_type: 1)
+    if !params[:search_term].nil?
+      @show_photos = flickr.photos.search(text: params[:search_term], page: params[:page], per_page: 10, content_type: 1)
+    end
   end
 
 end
