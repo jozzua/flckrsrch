213b16f7ca2973893c28d54206a9510a2dbed304
Elymar <jozzua@gmail.com>
2013-07-26 22:46:34 +0800

    Searches text instead of tag

--- app/controllers/photosearch_controller.rb
+++ app/controllers/photosearch_controller.rb
@@ -7,25 +7,7 @@ class PhotosearchController < ApplicationController
   def show
     @searchterm = params[:search_term]
     session[:searchterm] = @searchterm
-    @show_photos = flickr.photos.search(tags: params[:search_term], page: params[:page], per_page: 10, content_type: 1)
-    add_url
+    @show_photos = flickr.photos.search(text: params[:search_term], page: params[:page], per_page: 10, content_type: 1)
   end
 
-private 
-
-  def add_url
-    @show_photos.each{|i| i.to_hash.merge!("url" => generate_url(i)) }
-  end
-
-  def generate_url(i)
-    url = [prefix(i), i['server'], suffix(i)].join('/')
-  end
-
-  def prefix(i)
-     ['http://farm', i['farm'], '.staticflickr.com'].join('')
-  end
-
-  def suffix(i)
-    [i['id'], i['secret'], 'm'].join('_') + '.jpg'
-  end 
 end
--- spec/controllers/photosearch_controller_spec.rb
+++ spec/controllers/photosearch_controller_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
 describe PhotosearchController do
+  describe 'gets api for flickr' do
+    it 'hits flickr api' do
+      flickr_fetch = flickr.photos.search(:text => 'kittens')
+      flickr_fetch.should_not be_nil
+    end
+  end
   describe 'search photos' do
     it 'assigns a new search' do
       get :search, search_term: 'kittens'
       response.should be_ok
     end
-  end
+ end
 end
